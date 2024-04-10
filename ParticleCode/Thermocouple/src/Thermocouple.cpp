12565e88a2895bdf8aa1f99ab8bf6225c7ddc4e4
brashap <brashap@cnm.edu>
2024-04-10 10:16:14 -0600

    thermocouple

---
+++ ParticleCode/Thermocouple/src/Thermocouple.cpp
@@ -0,0 +1,34 @@
+/* 
+ * Project Thermocouple using MAX6675
+ * Author: Brian Rashap
+ * Date: 10-APR-2024
+ */
+
+// Include Particle Device OS APIs
+#include "Particle.h"
+#include "MAX6675.h"
+
+MAX6675 thermocouple;
+int status;
+
+SYSTEM_MODE(SEMI_AUTOMATIC);
+
+void setup () {
+  Serial.begin(9600);
+  waitFor(Serial.isConnected,10000);
+  thermocouple.begin(SCK, SS, MISO);
+}
+
+
+void loop () {
+  status = thermocouple.read();
+  if (status != STATUS_OK) {
+    Serial.printf("ERROR!\n");
+  }
+
+ // Read the raw Data value from the module
+  Serial.printf("RAW: %02X\n",thermocouple.getRawData());
+
+  Serial.printf("TMP: %0.3f\n",thermocouple.getTemperature());
+  delay(500);
+}
