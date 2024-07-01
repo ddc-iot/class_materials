8b185013935276c5bdef61a000c31b42bbf1ca79
Brian Rashap <brashap@cnm.edu>
2024-07-01 11:32:09 -0600

    updated I2C Scanner

--- ParticleCode/I2C_Scan/src/I2C_Scan.cpp
+++ ParticleCode/I2C_Scan/src/I2C_Scan.cpp
@@ -1,8 +1,3 @@
-/******************************************************/
-//       THIS IS A GENERATED FILE - DO NOT EDIT       //
-/******************************************************/
-
-#line 1 "c:/Users/IoT_Instructor/Documents/IoT/class_slides/ParticleCode/I2C_Scan/src/I2C_Scan.ino"
 /*
  * Project I2C_Scan
  * Description: Scan I2C bus and return found addresses
@@ -12,19 +7,18 @@
 
 #include "Particle.h"
 
-void setup();
-void loop();
-#line 10 "c:/Users/IoT_Instructor/Documents/IoT/class_slides/ParticleCode/I2C_Scan/src/I2C_Scan.ino"
 byte status; 
 byte address;
 int nDevices;
-unsigned long delayTime=2000;
+unsigned long delayTime=5000;
+
+SYSTEM_MODE(SEMI_AUTOMATIC);
 
 void setup() {
 	Wire.begin();
 	Serial.begin(9600);
-	while(!Serial);    // time to get serial running
-	Serial.println("\nI2C Scanner");
+	waitFor(Serial.isConnected,10000);
+	Serial.printf("I2C Scanner\n");
 }
 
 
